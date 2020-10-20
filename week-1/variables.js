06ea427aa5d4022ad48f39fda44dcbeb411f1318
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-10-19 22:57:29 -0400

    upload 1.1

---
+++ week-1/variables.js
@@ -0,0 +1,18 @@
+/* ============================================ 
+; Title: Assignment 1.1
+; Author: Jonathan Disla
+; Date: 19 October 2020
+; Modified By: Jonathan Disla
+; Description: This program demonstrates the ; use of JavaScript types, values, and
+; and variables in an application. 
+;=========================================== */
+
+//Below is an object with two properties, greeting and date.
+
+user = "John"
+var info : {
+    greeting: "Hello " + user,
+    date: new Date()
+}
+
+console.log(topInfo.greeting + " today is " + topInfo.date);
