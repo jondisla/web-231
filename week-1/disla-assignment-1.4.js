9e3ab7f8b41a3c076d8eba25876d30311e617594
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-10-20 21:14:29 -0400

    upload 1.4

---
+++ week-1/disla-assignment-1.4.js
@@ -0,0 +1,13 @@
+var firstName = "Jonathan";
+var lastName = "Disla";
+var age = "34";
+var street = "123 Fake Street";
+var city = "Springfield";
+var zipCode = "12224";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
