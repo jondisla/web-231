c1d3b77502e724f06eda6c87f34f6cb3d056d912
Jonathan Disla <jond@Jonathans-MacBook-Air.local>
2020-10-25 19:59:41 -0400

    1.5 upload

---
+++ week-1/disla-assignment-1.5.js
@@ -0,0 +1,51 @@
+/* 
+========================================== 
+; Title: Assignment 1.5
+; Author: Jonathan Disla 
+; Date: 25 October 2020 
+; Modified By: Jonathan Disla 
+; Description: Types, variables and values
+; ======================================== 
+*/
+
+//Employee #1
+
+var firstName = "Jim";
+var lastName = "Jones";
+var address = "123 Fake Street";
+var payRate = 30.50.toFixed(1);
+var hireDate = new Date("2016/01/24").toLocaleDateString();
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(address);
+// console.log(payRate);
+// console.log(hireDate);
+
+//Employee #2
+
+var firstName = "Paul";
+var lastName = "Stephens";
+var address = "480 Plant Street";
+var payRate = 45.10.toFixed(1);
+var hireDate = new Date(2020, 07, 15).toLocaleDateString();
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(address);
+// console.log(payRate);
+// console.log(hireDate);
+
+//Employee #3
+
+var firstName = "Keily";
+var lastName = "Noles";
+var address = "180 Washington Ave";
+var payRate = 35.25.toFixed(1);
+var hireDate = new Date(2012, 04, 02).toLocaleDateString();
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(address);
+//console.log(payRate);
+// console.log(hireDate);
