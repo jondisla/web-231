4289e4f03b7a7e3e8ddda22953999b9c884ab90f
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-10-29 22:09:08 -0400

    fixed formatting 2.3

--- week-2/disla-assignment-2.3.js
+++ week-2/disla-assignment-2.3.js
@@ -7,12 +7,22 @@
 ;===========================================
 */
 
+/**
+* header: constant that holds the import from disla-header.js file
+* console.log: logs the function from the disla-header.js file
+*/
+
+const header = require("../disla-header.js");
+console.log(header.display("Jonathan", "Disla", "Week 2"));
+
 /*The "myName" function assigns the string Jonathan to the property jonathan.
 *The property is then returned.
 */
+
 function myName(){
-  myName.jonathan = "Jonathan"
+  myName.jonathan = "Jonathan";
   return myName.jonathan;
  };
+
 //This console.log funciton outputs my first name coming from the myName fuction.
 console.log("My name is " + myName());
