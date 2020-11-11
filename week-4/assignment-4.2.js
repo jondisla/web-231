a1e546bbacd97870b6fbb5fa2565b868411ea085
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-11-11 17:26:29 -0500

    upload 4.2

---
+++ week-4/assignment-4.2.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  Pattern Matching Functions
+; Author: Jonathan Disla
+; Date:   02 November 2020
+; Description: Logical error handling
+;===========================================
+*/
+
+/**
+* header: constant that holds the import from disla-header.js file
+* console.log: logs the function from the disla-header.js file
+*/
+const header = require("../disla-header.js");
+console.log(header.display("Jonathan", "Disla", "Assignment 3.2"));
+
+/*
+===============================================
+BEGIN ASSIGNMENT
+===============================================
+*/
