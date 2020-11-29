460d8c7c9a99ff5d59f980ac2812ac391973fb94
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2020-11-28 19:37:51 -0500

    upload

--- week-6/disla-exercise-6.3.js
+++ week-6/disla-exercise-6.3.js
@@ -3,7 +3,7 @@
 ; Title:  Exercise 6.3 - Object Literals
 ; Author: Jonathan Disla
 ; Date:    November 28 2020
-; Description:
+; Description: Using setters and getters in object
 ;===========================================
 */
 
@@ -20,18 +20,22 @@ BEGIN ASSIGNMENT
 ===============================================
 */
 
+//create a tickets object with getters and setters
 let ticket = {
   id: 101,
   name: "Help Desk Support",
   requester: "Bob Jones",
 
+  //this getter gathers all the information and formats it accordingly
   get fullTicket(){
     return "{id: " + this.id + ", name: " + this.name + ", requester: " + this.requester+ "}";
   },
 
+  //the setter sets the value val
   set fullTicket(val){
     this.fullTicket = val;
   }
 }
 
+//log the property fullticket of the ticket object
 console.log(ticket.fullTicket)
